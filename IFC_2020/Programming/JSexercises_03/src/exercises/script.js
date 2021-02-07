141bd7b9de01b5b53819f6797fa464358388dbf1
Cristian Prochnow <cristianprochnow@gmail.com>
2021-02-07 16:24:26 -0300

    feat: finish exercise #2

--- IFC_2020/Programming/JSexercises_03/src/exercises/script.js
+++ IFC_2020/Programming/JSexercises_03/src/exercises/script.js
@@ -1,5 +1,7 @@
 Promise.resolve()
-  .then(() => exercise_01())
+  // .then(() => exercise_01())
+  // .then(() => exercise_02())
+  .then(() => exercise_03())
 
 function exercise_01() {
   const teams = []
@@ -26,7 +28,47 @@ function exercise_01() {
   alert(`O melhor time é o(a) ${bestTeam.name}`)
 }
 
-function exercise_02() {}
+function exercise_02() {
+  const citizenships = [
+    {
+      name: 'Carioca',
+      state: 'RJ'
+    },
+    {
+      name: 'Paulista',
+      state: 'SP'
+    },
+    {
+      name: 'Mineiro',
+      state: 'MG'
+    }
+  ]
+  let isUserAtOneOfTheCategories = false
+  let definedCitizenshipOfUser = ''
+
+  function askCitizenship() {
+    return String(prompt('Qual a sigla de seu estado onde você nasceu?'))
+  }
+
+  const stateAbbreviationOfLocalizationWhereUserCameFrom = askCitizenship()
+
+  for (let citizenship of citizenships) {
+    let isEqualCitizenship = citizenship.state === stateAbbreviationOfLocalizationWhereUserCameFrom
+
+    if (isEqualCitizenship) {
+      isUserAtOneOfTheCategories = true
+      definedCitizenshipOfUser = citizenship.name
+
+      break
+    }
+  }
+
+  if (isUserAtOneOfTheCategories) {
+    alert(`Olá, bem-vindo! Fico feliz que você seja ${definedCitizenshipOfUser}.`)
+  } else {
+    alert(`Desculpe, não sabemos de onde você é.`)
+  }
+}
 
 function exercise_03() {}
 
