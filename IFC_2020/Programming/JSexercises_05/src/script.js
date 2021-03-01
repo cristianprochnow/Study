5af71a4515e6f2ab8e4bf5b3d675ef638f317934
Cristian Prochnow <cristianprochnow@gmail.com>
2021-03-01 17:57:45 -0300

    feat: finish a couple of questions

--- IFC_2020/Programming/JSexercises_05/src/script.js
+++ IFC_2020/Programming/JSexercises_05/src/script.js
@@ -6,7 +6,11 @@ Promise.resolve()
   .then(exercise03)
   .then(() => exercise04(testNumbers))
   .then(() => exercise05(testNumbers))
-  .then(() => exercise06(25))
+  .then(exercise06)
+  .then(exercise07)
+  .then(exercise08)
+  .then(exercise09)
+  .then(exercise13)
 
 function separateSections(exerciseNumber) {
   console.warn(`Exercise #${exerciseNumber}`)
@@ -69,6 +73,8 @@ function exercise03() {
 }
 
 function exercise04(numbers) {
+  separateSections(4)
+
   let ageSum = 0,
     ageAmount = 20
 
@@ -82,6 +88,8 @@ function exercise04(numbers) {
 }
 
 function exercise05(numbers) {
+  separateSections(5)
+
   let negativeNumbersAmount = 0
 
   numbers.map(number => {
@@ -95,6 +103,213 @@ function exercise05(numbers) {
   })
 }
 
-function exercise06(availableInk, clickAmount) {
-  let inkAmountUsed
+function exercise06() {
+  separateSections(6)
+
+  let gastoTinta = 0.02, qtdTinta, qtdParcial, qtdTintaFinal, randomText;
+
+  qtdTinta = prompt("Informe o quanto de tinta ainda tem em sua caneta (mL):");
+  qtdTintaFinal =qtdTinta;
+
+  do {
+    qtdTinta = qtdTintaFinal;
+    randomText = prompt("Enquanto tem tinta, a caneta escreve o quê?");
+
+    qtdParcial = Number (qtdTinta * gastoTinta);
+
+    qtdTintaFinal = Number (qtdTinta - qtdParcial);
+
+    const qtdFinal = qtdTintaFinal.toFixed(2);
+
+    const mensagemFinal = `Ainda resta ${qtdFinal} mL de tinta...`;
+
+    alert(mensagemFinal)
+  } while (qtdTintaFinal > 0)
+
+  alert("Acabou a tinta da caneta!");
+}
+
+function exercise07() {
+  let smallestHeight = 0,
+    highestHeight = 0,
+    sumHeight = 0,
+    averageHeight = 0,
+    timesHaveAsked = 0,
+    hasGrantedContinuation = false,
+    height = 0,
+    highestID,
+    smallestID,
+    identity,
+    isFirstRound = true;
+
+  do {
+    identity = Number(prompt("Digite seu número de inscrição"));
+    height = Number(prompt("Digite sua altura"));
+
+    timesHaveAsked++;
+    sumHeight += height;
+
+    if (isFirstRound) {
+      smallestID = identity;
+      highestID = identity;
+      smallestHeight = height;
+      highestHeight = height;
+      isFirstRound = false;
+    }
+
+    if (height > highestHeight) {
+      highestHeight = height;
+      highestID = identity;
+    } else if (height < smallestHeight) {
+      smallestHeight = height;
+      smallestID = identity;
+    }
+
+    hasGrantedContinuation = prompt("Deseja continuar? [S]im ou [N]ão") !== "N" ? true : false;
+  } while (hasGrantedContinuation);
+
+  averageHeight = sumHeight / timesHaveAsked;
+
+  alert(`O atleta cujo número de inscrição é ${smallestID}, possui a menor altura (${smallestHeight})`);
+  alert(`O atleta cujo número de inscrição é ${highestID}, possui a maior altura (${highestHeight})`);
+  alert(`A média das ALTURAS é ${averageHeight.toFixed(2)}`);
+}
+
+function exercise08() {
+  let nome,
+    idade,
+    i,
+    contador = 0,
+    contadorFim = 0,
+    nomeRaiz = [];
+
+  for (i = 1; i <= 50; i++) {
+    contador = contadorFim;
+
+    nome = prompt("Informe seu nome:")
+    idade = prompt("Informe sua idade:")
+
+    if (idade == 18){
+      nomeRaiz.push(nome);
+    }
+
+    if (idade>20) {
+      contadorFim = contador + 1;
+    }
+  }
+
+  const mensagemFinal1 = `${nomeRaiz} são os alunos com 18 anos.`;
+  alert(mensagemFinal1);
+
+  const mensagemFinal2 = `Há ${contadorFim} alunos com mais de 20 anos.`;
+  alert(mensagemFinal2);
+}
+
+function exercise09() {
+  separateSections(9)
+
+  let calculations = [],
+    actualIndex = 0
+
+  for (let counter = 1; counter <= 5; counter += 0.1) {
+    actualIndex = Number(counter.toFixed(1))
+
+    calculations.push({
+      counter: actualIndex,
+      result: (
+        (3 + (2 * actualIndex) + ((6 * actualIndex) ** 2))
+        /
+        ((1 + (9 * actualIndex) + ((16 * actualIndex) ** 2)))
+      ).toFixed(5),
+      structure: `
+        (3 + 2(${actualIndex}) + 6(${actualIndex})^2)/(1 + 9(${actualIndex}) + 16(${actualIndex})^2)
+      `
+    })
+  }
+
+  console.log(calculations)
+}
+
+function exercise_10(){
+  separateSections(10)
+
+  let counter = 1,
+  hasFinished = false,
+  number = 0,
+  minNumber = 0,
+  maxNumber = 0,
+  isFirstRound = true
+
+  do {
+      number = Number(prompt(`Qual número você quer adicionar para a sequência!`))
+
+      counter ++
+
+      if (number === 0) {
+          hasFinished = true
+      }
+      if (isFirstRound) {
+          minNumber = number
+          maxNumber = number
+          isFirstRound = false
+      }
+      if (number > maxNumber && number !== 0) {
+          maxNumber = number
+      } else if (number < minNumber && number !== 0) {
+          minNumber = number
+      }
+  } while (!hasFinished)
+
+  alert(`max number = ${maxNumber} | min number = ${minNumber} `)
+}
+
+function exercise_11(){
+  separateSections(11)
+
+  let numberSum = 0,
+  numbersArray = []
+
+  for(let counter = 1; counter<=100; counter++){
+      numberSum += counter
+      numbersArray.push(counter)
+  }
+  alert(`NumberSum = ${numberSum}`)
+  alert(`${numbersArray.join(' + ')}`)
+}
+
+function exercise_12(){
+  separateSections(12)
+
+  let numberSum = 0,
+  numbersArray = []
+
+  for(let counter = 1; counter<=100; counter++){
+      numberSum += 1/counter
+      numbersArray.push(`1/${counter}`)
+  }
+  alert(`NumberSum = ${numberSum.toFixed(2)}`)
+  alert(`${numbersArray.join(' + ')}`)
+}
+
+function exercise13() {
+  separateSections(13)
+
+  const calculations = []
+  let calcResult = 0,
+    secondCounter = 0
+
+  const limitNumber = Number(prompt('Defina um valor numérico positivo para N:'))
+
+  secondCounter = limitNumber
+
+  for (let counter = 1; counter <= limitNumber; counter++) {
+    calcResult += counter / secondCounter
+
+    calculations.push(`${counter}/${secondCounter}`)
+
+    secondCounter--
+  }
+
+  alert(`Resultado do cálculo: ${calcResult}`)
+  alert(`Cálculo: [${calculations.join(' + ')}]`)
 }
